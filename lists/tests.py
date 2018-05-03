60d388bcc28a63a0e040ac6cbfc88b8a55fedda4
neoMarcos <nedokoro@rediffmail.com>
2018-05-03 11:27:51 -0300

    Modelo para lista de Itens e migrações associadas

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -16,3 +17,22 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelsTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_save_item = saved_items[0]
+        second_save_item = saved_items[1]
+        self.assertEqual(first_save_item.text, 'The first (ever) list item')
+        self.assertEqual(second_save_item.text, 'Item the second')
