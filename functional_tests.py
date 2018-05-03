5c3981061793de500cd028d5104a903b439212e2
neoMarcos <nedokoro@rediffmail.com>
2018-05-03 11:09:44 -0300

    Refactoring after 3 repeated lines

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Camilla gostaria de utilizar um aplicativa para suas listas. Ela
         # resolve entrar no site para testar.
@@ -37,24 +42,23 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-##        self.assertTrue(
-##            any(row.text == '1: Buy peacock feathers' for row in rows),
-##            f"New to-do item did not appear in table. Contents were:\n{table.text}"
-##        )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('The goat is watching. Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her lists
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to male a fly')
 
         # Edith wonders wheter the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        self.fail('The goat is watching. Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
